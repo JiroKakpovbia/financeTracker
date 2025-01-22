8bad6f706764bece8c747e7228112a41cf30f6fe
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-01-22 15:11:53 -0500

    initialized .NET MAUI project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace financeTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace financeTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
