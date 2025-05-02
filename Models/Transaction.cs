28d76131dab01023b21b7692e9a1adfcf7c66794
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-05-02 01:11:13 -0400

    made description and balance nullable

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -2,7 +2,7 @@
 public class Transaction
 {
     public DateTime Date { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public decimal Amount { get; set; }
-    public string Balance { get; set; }
+    public string? Balance { get; set; }
 }
