66094bd5e5f4b02a21b60dda1c9e78f54e981967
JiroKakpovbia <kakpovbia.jiro@gmail.com>
2025-08-11 15:36:44 -0400

    removed nullability warning for bool converter

--- Converters/InverseBoolConverter.cs
+++ Converters/InverseBoolConverter.cs
@@ -7,8 +7,8 @@ namespace financeTracker
     public class InverseBoolConverter : IValueConverter
     {
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => value is bool b ? !b : value;
+            => value is bool b ? !b : value!;
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => value is bool b ? !b : value;
+            => value is bool b ? !b : value!;
     }
 }
